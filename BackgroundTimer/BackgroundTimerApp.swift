6c9893a6fa2c555ddd4ef2ca270a1b454f5af06d
Brad Leege <bleege@gmail.com>
2023-02-11 14:43:49 -0600

    Initial Commit

---
+++ BackgroundTimer/BackgroundTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundTimerApp.swift
+//  BackgroundTimer
+//
+//  Created by Brad Leege on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
